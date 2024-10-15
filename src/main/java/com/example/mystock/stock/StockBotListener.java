92d33adcba88ec354a675df13445169b28287e17
Flash-KIM142 <topqr123q@gmail.com>
2024-10-15 23:55:01 +0900

    feat: add proper response for types of client's message
    - proper response for each cases for StockBotListener

--- src/main/java/com/example/mystock/stock/StockBotListener.java
+++ src/main/java/com/example/mystock/stock/StockBotListener.java
@@ -1,5 +1,9 @@
 package com.example.mystock.stock;
 
+import com.example.mystock.message.MessageJudge;
+import com.example.mystock.stock.model.request.WalletRequestDto;
+import com.example.mystock.stock.service.FavoriteService;
+import com.example.mystock.stock.service.WalletService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -13,6 +17,9 @@ import org.springframework.stereotype.Component;
 public class StockBotListener extends ListenerAdapter {
 
     private final PolygonClient polygonClient;
+    private final WalletService walletService;
+    private final FavoriteService favoriteService;
+    private final MessageJudge messageJudge;
 
     @Value("${polygon.api.key}")
     private String polygonApiKey;
@@ -23,10 +30,36 @@ public class StockBotListener extends ListenerAdapter {
             return;
         }
 
-        String symbol = event.getMessage().getContentRaw();
-        PolygonStockResponse polygonStockResponse = polygonClient.getPrevStock(symbol, polygonApiKey);
+        String message = event.getMessage().getContentRaw();
+        log.info("Received message {}", message);
 
+        MessageJudge.MessageTypeAndContent messageTypeAndContent = messageJudge.judge(message);
+        if(messageTypeAndContent.getMessageType()== MessageJudge.MessageType.UNKNOWN) {
+            event.getChannel().sendMessage("Invalid Message. Please send the message in given format.").queue();
+            return;
+        }
+
+        String ticker = messageTypeAndContent.getTicker();
+        log.info("Message Type {}", messageTypeAndContent.getMessageType());
+        log.info("ticker {}", ticker);
+        PolygonStockResponse polygonStockResponse = polygonClient.getPrevStock(ticker, polygonApiKey);
         String prevStockDescription = polygonStockResponse.getPrevStockDescription();
-        event.getChannel().sendMessage(prevStockDescription).queue();
+        String responseMessage;
+        switch (messageTypeAndContent.getMessageType()) {
+            case ADD_OWN_STOCK -> {
+                responseMessage = walletService.save(WalletRequestDto.of(messageTypeAndContent.getContent()),
+                        polygonStockResponse.getPrevClose());
+                event.getChannel().sendMessage(responseMessage).queue();
+            }
+            case ADD_FAV_STOCK -> {
+                responseMessage = favoriteService.save(messageTypeAndContent.getContent().get(0),
+                        prevStockDescription);
+                event.getChannel().sendMessage(responseMessage).queue();
+            }
+            case PREV_CLOSE -> {
+                responseMessage = polygonStockResponse.getPrevStockDescription();
+                event.getChannel().sendMessage(responseMessage).queue();
+            }
+        }
     }
 }
